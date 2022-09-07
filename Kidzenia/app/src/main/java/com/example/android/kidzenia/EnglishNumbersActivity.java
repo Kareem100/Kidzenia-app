3e5e7127713e58e9d36bbad5d3716056171cef68
Kareem100 <KareemSherif210@gmail.com>
2022-09-07 20:28:47 +0200

    performance: Closes issue #8

--- Kidzenia/app/src/main/java/com/example/android/kidzenia/EnglishNumbersActivity.java
+++ Kidzenia/app/src/main/java/com/example/android/kidzenia/EnglishNumbersActivity.java
@@ -3,35 +3,32 @@ package com.example.android.kidzenia;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class EnglishNumbersActivity extends AppCompatActivity {
+public class EnglishNumbersActivity extends AppCompatActivity
+        implements DataAdapter.OnListItemClickListener {
 
+    private ArrayList<Data> dataArrayList;
     private MediaPlayer mediaPlayer, music;
     private ImageView background;
-    private ListView listView;
     private boolean backPressed;
     protected static boolean VISIBLE_BACKGROUND = false, playMusic = false;
-    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+    private final MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
             releaseMedia();
-            if(background.getDrawable()!=null){
+            if (background.getDrawable() != null) {
                 background.animate().alpha(0).setDuration(1000);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        music.start();
-                        playMusic = true;
-                        background.setImageDrawable(null);
-                    }
+                new Handler().postDelayed(() -> {
+                    music.start();
+                    playMusic = true;
+                    background.setImageDrawable(null);
                 }, 2000);
             }
         }
@@ -47,54 +44,55 @@ public class EnglishNumbersActivity extends AppCompatActivity {
         backPressed = false;
 
         background = findViewById(R.id.display_data_background);
-        listView = findViewById(R.id.list_view);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false));
 
-        if(!VISIBLE_BACKGROUND) {
+        if (!VISIBLE_BACKGROUND) {
             background.animate().alpha(1).setDuration(1000);
             VISIBLE_BACKGROUND = true;
-            mediaPlayer = mediaPlayer.create(this, R.raw.sound_w);
+            mediaPlayer = MediaPlayer.create(this, R.raw.sound_w);
             mediaPlayer.start();
-            mediaPlayer.setVolume(0, 0);
             mediaPlayer.setOnCompletionListener(completionListener);
         }
 
-        final ArrayList<Data> arrayList = new ArrayList<Data>();
-        arrayList.add(new Data(R.raw.sound_1, R.drawable.number_1, R.drawable.apple_1, "One"));
-        arrayList.add(new Data(R.raw.sound_2, R.drawable.number_2, R.drawable.apples_2, "Two"));
-        arrayList.add(new Data(R.raw.sound_3, R.drawable.number_3, R.drawable.apples_3, "Three"));
-        arrayList.add(new Data(R.raw.sound_4, R.drawable.number_4, R.drawable.apples_4, "Four"));
-        arrayList.add(new Data(R.raw.sound_5, R.drawable.number_5, R.drawable.apples_5, "Five"));
-        arrayList.add(new Data(R.raw.sound_6, R.drawable.number_6, R.drawable.apples_6, "Six"));
-        arrayList.add(new Data(R.raw.sound_7, R.drawable.number_7, R.drawable.apples_7, "Seven"));
-        arrayList.add(new Data(R.raw.sound_8, R.drawable.number_8, R.drawable.apples_8, "Eight"));
-        arrayList.add(new Data(R.raw.sound_9, R.drawable.number_9, R.drawable.apples_9, "Nine"));
-        arrayList.add(new Data(R.raw.sound_10, R.drawable.number_10, R.drawable.apples_10, "Ten"));
+        dataArrayList = new ArrayList<>();
+        dataArrayList.add(new Data(R.raw.sound_1, R.drawable.number_1, R.drawable.apple_1, "One"));
+        dataArrayList.add(new Data(R.raw.sound_2, R.drawable.number_2, R.drawable.apples_2, "Two"));
+        dataArrayList.add(new Data(R.raw.sound_3, R.drawable.number_3, R.drawable.apples_3, "Three"));
+        dataArrayList.add(new Data(R.raw.sound_4, R.drawable.number_4, R.drawable.apples_4, "Four"));
+        dataArrayList.add(new Data(R.raw.sound_5, R.drawable.number_5, R.drawable.apples_5, "Five"));
+        dataArrayList.add(new Data(R.raw.sound_6, R.drawable.number_6, R.drawable.apples_6, "Six"));
+        dataArrayList.add(new Data(R.raw.sound_7, R.drawable.number_7, R.drawable.apples_7, "Seven"));
+        dataArrayList.add(new Data(R.raw.sound_8, R.drawable.number_8, R.drawable.apples_8, "Eight"));
+        dataArrayList.add(new Data(R.raw.sound_9, R.drawable.number_9, R.drawable.apples_9, "Nine"));
+        dataArrayList.add(new Data(R.raw.sound_10, R.drawable.number_10, R.drawable.apples_10, "Ten"));
 
-        DataAdapter dataAdapter = new DataAdapter(this, arrayList);
-        listView.setAdapter(dataAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                releaseMedia();
-                mediaPlayer = mediaPlayer.create(EnglishNumbersActivity.this, arrayList.get(position).getRawID());
-                mediaPlayer.start();
-                mediaPlayer.setOnCompletionListener(completionListener);
-            }
-        });
+        DataAdapter dataAdapter = new DataAdapter(this, dataArrayList, this);
+        recyclerView.setAdapter(dataAdapter);
     }
 
-    private void releaseMedia(){
+    @Override
+    public void onItemClickListener(int position) {
+        releaseMedia();
+        mediaPlayer = MediaPlayer.create(EnglishNumbersActivity.this,
+                dataArrayList.get(position).getRawID());
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(completionListener);
+    }
+
+    private void releaseMedia() {
         if (mediaPlayer != null) {
             mediaPlayer.release();
             mediaPlayer = null;
         }
     }
 
-    private void releaseMusic(){
+    private void releaseMusic() {
         music.release();
         music = null;
     }
+
     @Override
     public void onBackPressed() {
         finish();
@@ -106,21 +104,21 @@ public class EnglishNumbersActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(playMusic)
+        if (playMusic)
             music.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(!backPressed)
+        if (!backPressed)
             music.pause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(!backPressed){
+        if (!backPressed) {
             releaseMusic();
             releaseMedia();
         }
