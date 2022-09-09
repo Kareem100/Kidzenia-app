0234c1aedefb55b9de6828b3f6fd534942f7576e
Kareem100 <KareemSherif210@gmail.com>
2022-09-09 22:11:41 +0200

    frontend: Update the recycler views UI

--- Kidzenia/app/src/main/java/com/example/android/kidzenia/adapters/LettersAdapter.java
+++ Kidzenia/app/src/main/java/com/example/android/kidzenia/adapters/LettersAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.kidzenia.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.android.kidzenia.MainActivity;
 import com.example.android.kidzenia.models.LetterModel;
 import com.example.android.kidzenia.R;
 
 import java.util.ArrayList;
 
-public class LettersAdapter extends RecyclerView.Adapter<LettersAdapter.DataViewHolder> {
+public class LettersAdapter extends RecyclerView.Adapter<LettersAdapter.LetterViewHolder> {
 
     private final Context context;
     private final ArrayList<LetterModel> letterModelArrayList;
@@ -31,17 +33,17 @@ public class LettersAdapter extends RecyclerView.Adapter<LettersAdapter.DataView
 
     @NonNull
     @Override
-    public DataViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public LetterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(context).inflate(R.layout.layout_letters_list_item,
                 parent, false);
 
-        return new DataViewHolder(itemView, onListItemClickListener);
+        return new LetterViewHolder(itemView, onListItemClickListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DataViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull LetterViewHolder holder, int position) {
         LetterModel letterModelItem = letterModelArrayList.get(position);
-        holder.bind(context, letterModelItem.getLetterNumberID(),
+        holder.bind(letterModelItem.getLetterNumberID(),
                 letterModelItem.getAnimalAppleID(), letterModelItem.getName());
     }
 
@@ -50,14 +52,14 @@ public class LettersAdapter extends RecyclerView.Adapter<LettersAdapter.DataView
         return letterModelArrayList.size();
     }
 
-    public static class DataViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public static class LetterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final ImageView letterOrNumberView;
         private final ImageView animalOrAppleView;
         private final TextView itemNameView;
         private final OnListItemClickListener onListItemClickListener;
 
-        public DataViewHolder(@NonNull View itemView,
-                              OnListItemClickListener onListItemClickListener) {
+        public LetterViewHolder(@NonNull View itemView,
+                                OnListItemClickListener onListItemClickListener) {
             super(itemView);
             this.letterOrNumberView = itemView.findViewById(R.id.letters_numbers_gif);
             this.animalOrAppleView = itemView.findViewById(R.id.animals_apples_gif);
@@ -66,10 +68,16 @@ public class LettersAdapter extends RecyclerView.Adapter<LettersAdapter.DataView
             itemView.setOnClickListener(this);
         }
 
-        void bind(Context context, int letterNumberID, int animalAppleID, String itemNameTxt) {
-            Glide.with(context).asDrawable().load(letterNumberID).into(letterOrNumberView);
-            Glide.with(context).asDrawable().load(animalAppleID).into(animalOrAppleView);
+        @SuppressLint("SetTextI18n")
+        void bind(int letterNumberID, int animalAppleID, String itemNameTxt) {
+            Glide.with(itemView.getContext()).asDrawable().load(letterNumberID).into(letterOrNumberView);
+            Glide.with(itemView.getContext()).asDrawable().load(animalAppleID).into(animalOrAppleView);
             itemNameView.setText(itemNameTxt);
+
+            if (MainActivity.currentLocale == MainActivity.CurrentLocale.ARABIC)
+                ((TextView)itemView.findViewById(R.id.pronounce_text)).setText("تـهـجـأ");
+            else
+                ((TextView)itemView.findViewById(R.id.pronounce_text)).setText("Pronounce");
         }
 
         @Override
