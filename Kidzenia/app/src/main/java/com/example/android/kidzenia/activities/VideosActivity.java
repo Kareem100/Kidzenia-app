9f21df6e848de050369da7c9a687602c3da40bdd
Kareem100 <KareemSherif210@gmail.com>
2022-09-12 19:34:30 +0200

    enhancement: Add more educational videos to the current static list.

--- Kidzenia/app/src/main/java/com/example/android/kidzenia/activities/VideosActivity.java
+++ Kidzenia/app/src/main/java/com/example/android/kidzenia/activities/VideosActivity.java
@@ -25,14 +25,25 @@ public class VideosActivity extends AppCompatActivity implements VideosAdapter.O
         RecyclerView videosRecyclerView = findViewById(R.id.videos_recycler_view);
         videoModelArrayList = new ArrayList<>();
 
-        videoModelArrayList.add(getNewVideoModel("English Alphabet Song #1", "اغنية الحروف الابجدية الانجليزية #1", R.raw.english_alphabet_song));
-        videoModelArrayList.add(getNewVideoModel("English Numbers Song #1", "اغنية الارقام الانجليزية #1", R.raw.english_numbers_song));
-        videoModelArrayList.add(getNewVideoModel("Arabic Alphabet Song #1", "اغنية الحروف الابجدية العربية #1", R.raw.arabic_alphabet_song));
-        videoModelArrayList.add(getNewVideoModel("Arabic Numbers Song #1", "اغنية الارقام العربية #1", R.raw.arabic_numbers_song));
+        loadRawVideos();
 
         VideosAdapter videosAdapter = new VideosAdapter(this, videoModelArrayList, this);
         videosRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         videosRecyclerView.setAdapter(videosAdapter);
+    }
+
+    private void loadRawVideos() {
+        videoModelArrayList.add(getNewVideoModel("English Alphabet Song", "اغنية الحروف الابجدية الانجليزية", R.raw.english_alphabet_song));
+        videoModelArrayList.add(getNewVideoModel("English Numbers Song", "اغنية الارقام الانجليزية", R.raw.english_numbers_song));
+        videoModelArrayList.add(getNewVideoModel("English Alphabet Education", "الحروف الابجدية الانجليزية التعليمية", R.raw.english_arabic_alphabet_educ));
+        videoModelArrayList.add(getNewVideoModel("English Days of the Week Song", "اغنية ايام الاسبوع الانجليزية", R.raw.english_days_of_the_week_song));
+        videoModelArrayList.add(getNewVideoModel("English Never Give up Animation", "لا تستسلم ابدا (صور متحركة انجليزية)", R.raw.english_never_give_up_animation));
+
+        videoModelArrayList.add(getNewVideoModel("Arabic Alphabet Song", "اغنية الحروف الابجدية العربية", R.raw.arabic_alphabet_song));
+        videoModelArrayList.add(getNewVideoModel("Arabic Numbers Song", "اغنية الارقام العربية", R.raw.arabic_numbers_song));
+        videoModelArrayList.add(getNewVideoModel("Arabic Alphabet Train Song", "اغنية قطار الحروف الابجدية العربية", R.raw.arabic_alphabet_train_song));
+        videoModelArrayList.add(getNewVideoModel("Arabic Greedy Mouse Animation", "الفأر الطماع (صور متحركة عربية)", R.raw.arabic_greedy_mouse_animation));
+        videoModelArrayList.add(getNewVideoModel("Arabic Clean is Faith Animation", "النظافة من الايمان (صور متحركة عربية)", R.raw.arabic_clean_is_faith_animation));
     }
 
     private VideoModel getNewVideoModel(String title, String arabic_title, int raw_song_id) {
